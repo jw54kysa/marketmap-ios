93d9a84d1c62eaed704371dac062c2c8c9b4186b
jw54kysa <jonathan.weske@gmail.com>
2025-09-26 12:39:41 +0200

    Initial Commit

---
+++ XmasMarket/XmasMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  XmasMarketApp.swift
+//  XmasMarket
+//
+//  Created by Private Jon  on 26.09.25.
+//
+
+import SwiftUI
+
+@main
+struct XmasMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XmasMarketTests/XmasMarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  XmasMarketTests.swift
+//  XmasMarketTests
+//
+//  Created by Private Jon  on 26.09.25.
+//
+
+import Testing
+@testable import XmasMarket
+
+struct XmasMarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
